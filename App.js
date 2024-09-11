3dcbf28fc12e952d18cd99b6a5ba3202e9d31a96
zhuodp <zhuodp@fenbi.com>
2024-09-11 14:49:57 +0800

    RFCT: show only hello world in the main screen

---
+++ App.js
@@ -0,0 +1,18 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import { View, Text, SafeAreaView } from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>Hello World!</Text>
+    </SafeAreaView>
+  );
+};
+export default App;
