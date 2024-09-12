07bf946ac33f782a6a867a303db6d8c6ae58e14d
zhuodp <zhuodp@fenbi.com>
2024-09-12 16:47:10 +0800

    ADD: useState

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@
  * @format
  */
 
-import React from 'react';
+import React, {useState} from 'react';
 import { View, Text, SafeAreaView } from 'react-native';
 import MyText from './components/MyText';
 import styles from './components/styles';
@@ -14,13 +14,17 @@ import Toast from "react-native-toast-message";
 
 
 const App = () => {
+
+  const [textWithState, setText] = useState('original state')
+
   return (
     <SafeAreaView>
       <View style={styles.normalBox}><Text>this is the header</Text></View>
       <MyText/>
       <View style={styles.normalBox}><Text>this is the footer</Text></View>
-      <Toast/>
       <ItemView name={'table'} price={'$21'}></ItemView>
+
+      <Text onPress={() => setText('i switched to a new state now')}>{textWithState}</Text>
     </SafeAreaView>
   );
 };
