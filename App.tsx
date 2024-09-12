f7587fe94acf8fefb0122452c654c83a0edaeb59
zhuodp <zhuodp@fenbi.com>
2024-09-12 16:12:15 +0800

    ADD: click listener on MyText and event handling

--- App.tsx
+++ App.tsx
@@ -10,6 +10,8 @@ import { View, Text, SafeAreaView } from 'react-native';
 import MyText from './components/MyText';
 import styles from './components/styles';
 import ItemView from './components/ItemView';
+import Toast from "react-native-toast-message";
+
 
 const App = () => {
   return (
@@ -17,7 +19,7 @@ const App = () => {
       <View style={styles.normalBox}><Text>this is the header</Text></View>
       <MyText/>
       <View style={styles.normalBox}><Text>this is the footer</Text></View>
-      
+      <Toast/>
       <ItemView name={'table'} price={'$21'}></ItemView>
     </SafeAreaView>
   );
--- components/MyText.js
+++ components/MyText.js
@@ -1,9 +1,17 @@
-import React from "react"
-import { Text } from "react-native"
+import React from "react";
+import { Alert, Text } from "react-native";
 import styles from "./styles";
 
 const MyText = () => {
-    return <Text style={[styles.bigRed]}>This is the body text</Text>;
+    const handleClick = () => {
+        Alert.alert('Alert', 'The text has been clicked');
+    };
+
+    return (
+        <Text style={[styles.bigRed]} onPress={handleClick}>
+            This is the body text3
+        </Text>
+    );
 };
 
 export default MyText;
